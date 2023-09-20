465efaec4d84cc4f75db57d8d6a9296b04ee67f8
Jokubas Gedminas <jokas3@gmail.com>
2023-09-21 00:03:57 +0200

    Exercise 6 complete

---
+++ Ex6_Blackjack.java
@@ -0,0 +1,109 @@
+import java.util.Objects;
+
+public class Blackjack {
+
+    public int parseCard(String card) {
+
+        switch(card) {
+
+            case "ace":
+
+                return 11;
+            case "two":
+
+                return 2;
+            case "three":
+
+                return 3;
+            case "four":
+
+                return 4;
+            case "five":
+
+                return 5;
+            case "six":
+
+                return 6;
+            case "seven":
+
+                return 7;
+            case "eight":
+
+                return 8;
+            case "nine":
+
+                return 9;
+        }
+
+            if (Objects.equals(card, "ten") || Objects.equals(card, "jack") || Objects.equals(card, "queen") || Objects.equals(card, "king")) {
+
+                return 10;
+            }
+
+        return 0;
+    }
+
+    public boolean isBlackjack(String card1, String card2) {
+
+        if(Math.addExact(parseCard(card1), parseCard(card2)) == 21) {
+
+            return true;
+        }
+
+        return false;
+    }
+
+    public String largeHand(boolean isBlackjack, int dealerScore) {
+
+        if(isBlackjack && dealerScore < 10) {
+
+            return "W";
+        }
+
+        if(isBlackjack && dealerScore >= 10){
+
+            return "S";
+        }
+
+        return "P";
+    }
+
+    public String smallHand(int handScore, int dealerScore) {
+
+        if (handScore >= 17) {
+
+            return "S";
+        }
+
+        if(handScore >= 12 && handScore <= 16) {
+
+            if(dealerScore >= 7) {
+
+                return "H";
+            }
+
+            return "S";
+        }
+
+        if(handScore <= 11) {
+
+            return "H";
+        }
+
+        return "S";
+    }
+
+    // FirstTurn returns the semi-optimal decision for the first turn, given the cards of the player and the dealer.
+    // This function is already implemented and does not need to be edited. It pulls the other functions together in a
+    // complete decision tree for the first turn.
+    public String firstTurn(String card1, String card2, String dealerCard) {
+        int handScore = parseCard(card1) + parseCard(card2);
+        int dealerScore = parseCard(dealerCard);
+
+        if (20 < handScore) {
+            return largeHand(isBlackjack(card1, card2), dealerScore);
+        } else {
+            return smallHand(handScore, dealerScore);
+        }
+    }
+}
